b9f6c98c610d2fe4649a3cb15926eba31a051832
Leonardo Campos <leonardo.stenio@gmail.com>
2020-02-28 09:15:58 -0300

    Subindo o código

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://root:root@cluster0-g3vqc.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
